c1fde0de7f71684e627bfe33848804969a9b165d
BaeKY <ksc2601@gmail.com>
2020-08-28 14:40:43 +0900

    ErrorClass

--- src/api/Error/shared/Error.type.ts
+++ src/api/Error/shared/Error.type.ts
@@ -1,17 +1,41 @@
 import { Field, ObjectType } from "type-graphql";
+import { ValidationError } from "class-validator";
 import { ErrorCodes } from "./ErrorCodes";
 
 @ObjectType({ description: "Display error to client-side." })
 export class UserError {
-    constructor(message: string, code?: ErrorCodes) {
+    constructor(
+        message: string,
+        code?: ErrorCodes,
+        validationErrors?: ValidationError[]
+    ) {
         this.message = message;
         if (code) {
             this.code = code;
         }
+        if (validationErrors) {
+            console.log(JSON.stringify(validationErrors));
+            this.details = validationErrors.flatMap((err): string[] => {
+                const messages: string[] = [];
+                if (err.constraints) {
+                    for (const key in err.constraints) {
+                        const message = err.constraints[key];
+                        messages.push(
+                            `[${err.target?.constructor.name}.${err.property}] ${message} (validator=${key}, input=${err.value})`
+                        );
+                    }
+                }
+                return messages;
+            });
+        }
     }
-    @Field(() => String)
+
+    @Field(() => String, { defaultValue: "UNKNOWN_ERROR" as ErrorCodes })
     code: string;
 
     @Field(() => String)
     message: string;
+
+    @Field(() => [String], { description: "에러 디테일들... ", nullable: true })
+    details: string[];
 }
