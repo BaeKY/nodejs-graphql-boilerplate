73f67aa325ba9ca12d12eb02877e0079c207ad12
BaeKY <ksc2601@gmail.com>
2021-02-07 19:00:46 +0900

    Refactor Core service

--- src/modules/Core/Core.service.ts
+++ src/modules/Core/Core.service.ts
@@ -1,16 +1,17 @@
-import { ReturnModelType } from "@typegoose/typegoose";
+import { DocumentType, ReturnModelType } from "@typegoose/typegoose";
 import { ObjectId } from "mongodb";
 import { ClientSession } from "mongoose";
-import { OffsetPaging, PageInput } from "../Common/OffsetPaging.type";
+import { ClassType } from "type-graphql";
 
-export const BasicService = <T>(
-    cls: { new (...args: any): T },
-    model: ReturnModelType<typeof cls>
+export const BasicService = <T, CI_T, UI_T>(
+    cls: ClassType<T>,
+    model: ReturnModelType<typeof cls>,
+    inputTypes: {
+        createInput: ClassType<CI_T>;
+        updateInput?: ClassType<UI_T>;
+    }
 ) => {
-    const Pagination = OffsetPaging(cls);
-    type Pagination = InstanceType<typeof Pagination>;
-
-    abstract class CustomService {
+    abstract class AbsService {
         protected readonly model: ReturnModelType<typeof cls>;
 
         constructor() {
@@ -22,17 +23,21 @@ export const BasicService = <T>(
         }
 
         async create(
-            createInput: Partial<T>,
-            session?: ClientSession
-        ): Promise<T> {
+            createInput: InstanceType<typeof inputTypes.createInput>,
+            session?: ClientSession,
+            beforeSave?: (instance: DocumentType<T>) => Promise<void>
+        ): Promise<DocumentType<T>> {
             const instance = new this.model(createInput);
+            if (beforeSave) {
+                await beforeSave(instance);
+            }
             await instance.save({ session });
             return instance;
         }
 
         async updateById(
             _id: ObjectId,
-            updateInput: Partial<T>,
+            updateInput: UI_T,
             session?: ClientSession
         ) {
             return this.model.updateOne(
@@ -47,26 +52,6 @@ export const BasicService = <T>(
         async delete(id: ObjectId, session?: ClientSession) {
             return this.model.findByIdAndDelete(id, { session });
         }
-
-        async findWithPaging(
-            pageInput: PageInput,
-            q: {
-                filter?: any;
-                sort?: string[];
-                query?: any;
-            } = {}
-        ): Promise<Pagination> {
-            const result = new Pagination();
-            const { filter, query, sort } = q;
-            result.setData(this.model, {
-                pageNumber: pageInput.pageNumber,
-                pageItemCount: pageInput.pageItemCount,
-                filter,
-                sort,
-                query,
-            });
-            return result;
-        }
     }
-    return CustomService;
+    return AbsService;
 };
